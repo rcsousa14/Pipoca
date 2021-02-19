0bd0b84ebcb4640c9e4acf7f9dac922eb4a9a558
rcsousa <rcsousa14@hotmail.com>
2021-02-19 12:29:52 +0100

    changes the server subdomain will only need it when I have a domain name

--- pipoca-db/api/server.js
+++ pipoca-db/api/server.js
@@ -1,4 +1,4 @@
-import 'dotenv/config'; 
+import 'dotenv/config';
 import express from 'express';
 const morgan = require('morgan');
 import helmet from 'helmet';
@@ -15,9 +15,8 @@ app.set('trust proxy', 1);
 const port = process.env.PORT || 3000;
 
 const accessLogStream = fs.createWriteStream(
-    path.join(__dirname, '../access.log'),
-    { flags: 'a' }
-  );
+    path.join(__dirname, '../access.log'), { flags: 'a' }
+);
 
 
 app.set('view engine', 'ejs');
@@ -27,19 +26,19 @@ app.use(cors());
 app.use(morgan('combined', { stream: accessLogStream }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-
-app.use(subdomain('api', routes.user));
+app.use(routes.user);
+//app.use(subdomain('api', routes.user));
 
 
 app.use((req, res) => {
     res.status(404).render('404');
-     
-}); 
+
+});
 
 
 
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`
 ‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗
       __________  ____  __________  __________  __________  __________
