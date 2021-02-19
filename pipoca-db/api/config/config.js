67536409ad1da117193d1e34bd614138544af4d5
rcsousa <rcsousa14@hotmail.com>
2021-02-19 13:15:17 +0100

    another change to config

--- pipoca-db/api/config/config.js
+++ pipoca-db/api/config/config.js
@@ -15,6 +15,12 @@ module.exports = {
     },
     production: {
         use_env_variable: process.env.DATABASE_URL,
-
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     }
 }
