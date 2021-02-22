9b8fd8ef8ec57c252554de4dd076c951b1f99037
rcsousa <rcsousa14@hotmail.com>
2021-02-22 12:48:31 +0100

    added underscored for the production env

--- pipoca-db/api/config/config.js
+++ pipoca-db/api/config/config.js
@@ -16,6 +16,10 @@ module.exports = {
     production: {
         use_env_variable: "DATABASE_URL",
         dialect: 'postgres',
+        define: {
+            timestamps: true,
+            underscored: true,
+        },
         dialectOptions: {
             ssl: {
                 require: true,
