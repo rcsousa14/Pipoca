707a51acb3e831c595afe7775145ba8c5bedc128
rcsousa <rcsousa14@hotmail.com>
2021-02-22 15:17:59 +0100

    correcting auth controller admin to regular

--- pipoca-db/api/controllers/auth_controller.js
+++ pipoca-db/api/controllers/auth_controller.js
@@ -32,7 +32,7 @@ exports.signup = async (req, res) => {
             }
 
         }
-        const { id } = await models.role.findOne({ where: { role: 'admin' } })
+        const { id } = await models.role.findOne({ where: { role: 'regular' } })
         const newUser = await models.user.create({ username, phone_number, phone_carrier, birthday, avatar, bio, fcm_token, role_id: id });
         const token = auth.jwtToken.createToken(newUser);
 
