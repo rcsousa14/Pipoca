458582025ec42b1db4599c6115078d61b8256b5b
rcsousa <rcsousa14@hotmail.com>
2021-03-26 13:19:23 +0100

    test link preview11

--- pipoca-db/api/middleware/link_auth.js
+++ pipoca-db/api/middleware/link_auth.js
@@ -1,22 +1,50 @@
-import { getLinkPreview } from 'link-preview-js';
-
+import { getLinkPreview } from "link-preview-js";
 
 export default ({ body }, res, next) => {
     const { links } = body;
 
-
     if (!Array.isArray(links) || !links.length) {
         next();
     }
+    var nsfws = [
+        "porn",
+        "porno",
+        "pornografia",
+        "sex",
+        "sexo",
+        "xxx",
+        "pornhub",
+        "xvideos",
+        "xhamster",
+        "tube8",
+        "redtube",
+        "xnxx",
+        "hclips",
+        "spankbang",
+        "adult",
+        "slut",
+        "bitch",
+        "fuck",
+        "foder",
+        "porra",
+        "pussy",
+        "dick",
+        "ass",
+        "pila",
+        "cona",
+        "suicidio",
+        "brazzers",
+        "masturbation",
+        "masturb",
 
+    ];
 
+    if (nsfws.some((nsfw) => links[0].includes(nsfw))) {
+        return res.status(403).send({
+            message: "Desculpe,você não pode postar conteúdo adulto "
+        })
+    }
+    getLinkPreview(links[0]).then((data) =>
+        res.send(data));
 
-    getLinkPreview(links[0]).then((data) => res.send(data));
-
-
-
-
-
-
-
-}
+};
