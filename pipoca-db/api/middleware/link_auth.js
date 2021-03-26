abc65bddb15e5d68e81022fab21df7fdb628d8b4
rcsousa <rcsousa14@hotmail.com>
2021-03-26 12:48:03 +0100

    test link preview8

--- pipoca-db/api/middleware/link_auth.js
+++ pipoca-db/api/middleware/link_auth.js
@@ -1,24 +1,21 @@
 import { getLinkPreview } from 'link-preview-js';
-const isPorn = require('is-porn');
+
 
 export default ({ body }, res, next) => {
     const { links } = body;
-    let isStatus;
+
     let urlData = {};
     if (!Array.isArray(links) || !links.length) {
         next();
     }
 
 
-    isPorn(links[0], function(error, status) {
-        if (!error) return res.send(status)
-        return res.send(error)
-    });
-    // getLinkPreview(links[0]).then((data) => urlData = data);
+
+    getLinkPreview(links[0]).then((data) => urlData = data);
 
 
 
-
+    return res.send(urlData);
 
 
 
