c8b1a781b385e0effd5ef6e9e0ad14d73fb1cd5d
rcsousa <rcsousa14@hotmail.com>
2021-03-30 12:08:33 +0100

    add here

--- pipoca-db/api/middleware/link_auth.js
+++ pipoca-db/api/middleware/link_auth.js
@@ -1,82 +1,82 @@
-
-
-
 export default ({ body }, res, next) => {
     const { links } = body;
+    if (links) {
+        if (!Array.isArray(links) || !links.length) {
+            next();
+        }
 
-    if (!Array.isArray(links) || !links.length) {
+        var nsfws = [
+            "porn",
+            "porno",
+            "pornografia",
+            "sex",
+            "sexo",
+            "xxx",
+            "pornhub",
+            "xvideos",
+            "xhamster",
+            "tube8",
+            "redtube",
+            "xnxx",
+            "hclips",
+            "spankbang",
+            "adult",
+            "slut",
+            "bitch",
+            "fuck",
+            "foder",
+            "porra",
+            "pussy",
+            "dick",
+            "ass",
+            "pila",
+            "cona",
+            "suicidio",
+            "brazzers",
+            "masturbation",
+            "masturb",
+            "teamSkeet",
+            "mofos",
+            "pureTaboo",
+            "blacked",
+            "jizz",
+            "camsoda",
+            "chaturbate",
+            "anal",
+            "anus",
+            "puta",
+            "punheta",
+            "rabuda",
+            "xuxuta",
+            "xuxuado",
+            "Siririca",
+            "Testuda",
+            "Tezao",
+            "Tezuda",
+            "Tezudo",
+            "Xota",
+            "Xochota",
+            "Xoxota",
+            "Xana",
+            "Xaninha",
+            "liveleaks"
+
+
+
+        ];
+
+        if (nsfws.some((nsfw) => links[0].includes(nsfw))) {
+            return res.status(403).send({
+                message: "Desculpe, não pode postar conteúdo adulto",
+
+            })
+        }
+
+
+
         next();
+
     }
-
-    var nsfws = [
-        "porn",
-        "porno",
-        "pornografia",
-        "sex",
-        "sexo",
-        "xxx",
-        "pornhub",
-        "xvideos",
-        "xhamster",
-        "tube8",
-        "redtube",
-        "xnxx",
-        "hclips",
-        "spankbang",
-        "adult",
-        "slut",
-        "bitch",
-        "fuck",
-        "foder",
-        "porra",
-        "pussy",
-        "dick",
-        "ass",
-        "pila",
-        "cona",
-        "suicidio",
-        "brazzers",
-        "masturbation",
-        "masturb",
-        "teamSkeet",
-        "mofos",
-        "pureTaboo",
-        "blacked",
-        "jizz",
-        "camsoda",
-        "chaturbate",
-        "anal",
-        "anus",
-        "puta",
-        "punheta",
-        "rabuda",
-        "xuxuta",
-        "xuxuado",
-        "Siririca",
-        "Testuda",
-        "Tezao",
-        "Tezuda",
-        "Tezudo",
-        "Xota",
-        "Xochota",
-        "Xoxota",
-        "Xana",
-        "Xaninha",
-        "liveleaks"
-
-
-
-    ];
-
-    if (nsfws.some((nsfw) => links[0].includes(nsfw))) {
-        return res.status(403).send({
-            message: "Desculpe, não pode postar conteúdo adulto",
-
-        })
-    }
-   
-    
-   
     next();
 
 };
