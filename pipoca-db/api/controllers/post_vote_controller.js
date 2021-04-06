c949f208d6b679346ebc63357c586ee7af8841f1
rcsousa <rcsousa14@hotmail.com>
2021-04-06 10:52:15 +0100

    working on post points

--- pipoca-db/api/controllers/post_vote_controller.js
+++ pipoca-db/api/controllers/post_vote_controller.js
@@ -1,31 +1,31 @@
 const models = require('../models');
 
-exports.store = async({ body, decoded }, res, ) => {
+exports.store = async({ body, decoded }, res) => {
     try {
-
+        const { id } = decoded;
         const { postId, voted } = body;
         const post = await models.post.findByPk(postId);
         if (!post) {
             return res.status(400).send({ message: '🤔 Bago não foi encontrado!' });
         }
         const vote = await models.post_vote.findOne({
-            where: { user_id: decoded.id, post_id: postId }
+            where: { user_id: id, post_id: postId }
         });
 
         if (vote) {
-            const updated_vote = await models.post_vote.update({ userId: decoded.id, voted, post_id: postId  }, {
+            const updated_vote = await models.post_vote.update({ userId: id, voted, post_id: postId }, {
 
                 where: { id: vote.id },
             });
-            cache.del(`post_${decoded.id}`);
+            cache.del(`post_${id}`);
 
-            cache.del(`user_posts_${decoded.id}`);
+
             return res.status(200).send({ message: "updated", updated_vote });
         }
-        const add_vote = await models.post_vote.create({ userId: decoded.id,  post_id: postId, voted });
-        cache.del(`post_${decoded.id}`);
+        const add_vote = await models.post_vote.create({ userId: id, post_id: postId, voted });
+        cache.del(`post_${id}`);
 
-        cache.del(`user_posts_${decoded.id}`);
+
         return res.status(201).send({ message: 'added', add_vote });
 
 
