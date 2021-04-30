4425d5e169b05dc6f999398a2f81db32a37d5b91
rcsousa <rcsousa14@hotmail.com>
2021-04-30 14:37:58 +0100

    finish the singluar post working on comments fixed final2

--- pipoca-db/api/controllers/post_controller.js
+++ pipoca-db/api/controllers/post_controller.js
@@ -133,7 +133,7 @@ exports.show = async({ params, query, decoded }, res, next) => {
             linkInfo = await scrapeMetaTags(url);
         }
 
-        let post_info = {
+        let data = {
             user_voted: isVoted,
             user_vote: votes == null ? 0 : votes.voted,
             user_isNear: isNear,
@@ -151,7 +151,7 @@ exports.show = async({ params, query, decoded }, res, next) => {
             },
         };
 
-        const post = { success: true, message: ` Bago ${id} para ti`, post_info };
+        const post = { success: true, message: ` Bago ${id} para ti`, data };
 
         return res.status(200).json(post);
     } catch (error) {
