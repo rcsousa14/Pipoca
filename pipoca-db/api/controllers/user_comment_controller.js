547fc0436b87f5addead12c4a2d1bbf1aa8c60da
rcsousa <rcsousa14@hotmail.com>
2021-05-03 09:43:21 +0100

    comments not done yet

--- pipoca-db/api/controllers/user_comment_controller.js
+++ pipoca-db/api/controllers/user_comment_controller.js
@@ -16,58 +16,61 @@ exports.store = async({ params, body, decoded }, res, next) => {
             where: {
                 content: content,
                 user_id: decoded.id,
+                post_id: post_id,
                 createdAt: {
                     [Op.gt]: NOW,
                     [Op.lt]: TODAY_START,
                 }
             }
         });
-
-
-        if (result) {
-            next(ApiError.badRequestException("Ninguém gosta de spam"));
-            return;
-        }
-        var point = {
-            type: "Point",
-            coordinates: [longitude, latitude],
-            crs: { type: "name", properties: { name: "EPSG:4326" } },
-        };
-
-        const comment = await models.comment.create({
-            user_id: decoded.id,
-            post_id: post_id,
-            content,
-            coordinates: point,
-        });
-
-        if (!hashes.length == 0 && hashes.length > 0) {
-            for (var hash of hashes) {
-                const [tag] = await models.tag.findOrCreate({
-                    where: { hash: hash },
-                });
-
-                await models.post_tag.create({
-                    comment_id: comment.id,
-                    tag_id: tag.id,
-                });
-            }
-        }
-        if (!links.length == 0 && links.length > 0) {
-            const [link] = await models.link.findOrCreate({
-                where: { url: links[0] },
-            });
-
-            await models.post_link.create({
-                comment_id: comment.id,
-                link_id: link.id,
-            });
-        }
-
         return res.status(201).json({
-            success: true,
-            message: "Comentário criado com sucesso!",
-        });
+            result
+        })
+
+        // if (result) {
+        //     next(ApiError.badRequestException("Ninguém gosta de spam"));
+        //     return;
+        // }
+        // var point = {
+        //     type: "Point",
+        //     coordinates: [longitude, latitude],
+        //     crs: { type: "name", properties: { name: "EPSG:4326" } },
+        // };
+
+        // const comment = await models.comment.create({
+        //     user_id: decoded.id,
+        //     post_id: post_id,
+        //     content,
+        //     coordinates: point,
+        // });
+
+        // if (!hashes.length == 0 && hashes.length > 0) {
+        //     for (var hash of hashes) {
+        //         const [tag] = await models.tag.findOrCreate({
+        //             where: { hash: hash },
+        //         });
+
+        //         await models.post_tag.create({
+        //             comment_id: comment.id,
+        //             tag_id: tag.id,
+        //         });
+        //     }
+        // }
+        // if (!links.length == 0 && links.length > 0) {
+        //     const [link] = await models.link.findOrCreate({
+        //         where: { url: links[0] },
+        //     });
+
+        //     await models.post_link.create({
+        //         comment_id: comment.id,
+        //         link_id: link.id,
+        //     });
+        // }
+
+        // return res.status(201).json({
+        //     success: true,
+        //     message: "Comentário criado com sucesso!",
+        // });
     } catch (error) {
 
         next(
