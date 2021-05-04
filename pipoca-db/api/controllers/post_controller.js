1357a7fbdbb200f0407d78d3b95c854d59b4a76d
rcsousa <rcsousa14@hotmail.com>
2021-05-04 13:57:23 +0100

    minor changes to single post testing 22

--- pipoca-db/api/controllers/post_controller.js
+++ pipoca-db/api/controllers/post_controller.js
@@ -74,57 +74,57 @@ exports.show = async({ params, query, decoded }, res, next) => {
 
 
 
-        // let distance;
-        // if (lat && lng) {
-        //     distance = getDistance({ latitude: lat, longitude: lng }, {
-        //         latitude: posts.coordinates.coordinates[1],
-        //         longitude: posts.coordinates.coordinates[0],
-        //     });
-        // }
-        // let isNear;
-        // if (distance <= 950) isNear = true;
-        // if (distance > 950) isNear = false;
+        let distance;
+        if (lat && lng) {
+            distance = getDistance({ latitude: lat, longitude: lng }, {
+                latitude: posts.coordinates.coordinates[1],
+                longitude: posts.coordinates.coordinates[0],
+            });
+        }
+        let isNear;
+        if (distance <= 950) isNear = true;
+        if (distance > 950) isNear = false;
 
 
 
-        // let linkInfo = {};
+        let linkInfo = {};
 
 
 
 
-        // if (posts.links.length > 0) {
-        //     const { url } = posts.links[0];
+        if (posts.links.url != null) {
+            const { url } = posts.links.url;
 
-        //     linkInfo = await scrapeMetaTags(url);
-        // }
+            linkInfo = await scrapeMetaTags(url);
+        }
 
-        // let data = {
-        //     user_voted: vote ? true : false,
-        //     user_vote: vote == null ? 0 : vote,
-        //     user_isNear: isNear,
-        //     info: {
-        //         id: posts.id,
-        //         content: posts.content,
-        //         links: linkInfo,
-        //         votes_total: posts.votes_total == null ? 0 : posts.votes_total,
-        //         comments_total: posts.comments_total,
-        //         flags: posts.flags,
-        //         is_flagged: posts.is_flagged,
-        //         created_at: posts.createdAt,
-        //         creator: posts.creator,
-        //     },
-        // };
+        let data = {
+            user_voted: vote == null ? false : true,
+            user_vote: vote == null ? 0 : vote,
+            user_isNear: isNear,
+            info: {
+                id: posts.id,
+                content: posts.content,
+                links: linkInfo,
+                votes_total: posts.votes_total == null ? 0 : posts.votes_total,
+                comments_total: posts.comments_total,
+                flags: posts.flags,
+                is_flagged: posts.is_flagged,
+                created_at: posts.createdAt,
+                creator: posts.creator,
+            },
+        };
 
 
 
-        const post = { success: true, message: ` Bago ${id} para ti`, posts };
+        const post = { success: true, message: ` Bago ${id} para ti`, data };
 
         return res.status(200).json(post);
     } catch (error) {
-        return res.status(500).json(error);
-        // next(
-        //     ApiError.internalException("Não conseguiu se comunicar com o servidor")
-        // );
-        // return;
+
+        next(
+            ApiError.internalException("Não conseguiu se comunicar com o servidor")
+        );
+        return;
     }
 };
