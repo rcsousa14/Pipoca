9e2bd1c9685781358b6cb772b80b314a5d7af3d2
rcsousa <rcsousa14@hotmail.com>
2021-05-05 08:51:33 +0100

    testing sub2

--- pipoca-db/api/controllers/user_sub_comment_controller.js
+++ pipoca-db/api/controllers/user_sub_comment_controller.js
@@ -14,8 +14,8 @@ exports.store = async({ params, body, decoded }, res, next) => {
         const result = await models.sub_comment.findAll({
             where: {
                 content: content,
-                // user_id: decoded.id,
-                // comment_id: comment_id,
+                user_id: decoded.id,
+                comment_id: comment_id,
                 // createdAt: {
                 //     [Op.lt]: NOW,
                 //     [Op.gt]: TODAY_START,
