48e2a41e92e5a736c00753c8a1bc4dad4f2f7986
rcsousa <rcsousa14@hotmail.com>
2021-05-05 09:52:36 +0100

    testing sub get3

--- pipoca-db/api/controllers/user_sub_comment_controller.js
+++ pipoca-db/api/controllers/user_sub_comment_controller.js
@@ -1,8 +1,8 @@
 import ApiError from "../errors/api_error";
 
+const { paginate } = require("../utils/paginate");
 const models = require("../models");
 const Sequelize = require("sequelize");
-const { paginate } = require("../utils/paginate");
 
 const Op = Sequelize.Op;
 exports.store = async({ params, body, decoded }, res, next) => {
@@ -81,88 +81,88 @@ exports.store = async({ params, body, decoded }, res, next) => {
 exports.index = async({ params, query, decoded }, res, next) => {
     try {
         const { comment_id } = params;
-        const { lat, lng } = query;
-        const id = decoded.id;
-        const page = parseInt(query.page);
-        const limit = 4;
-        let group = ["sub_comment.id"];
-        let search = {
-            where: { comment_id: comment_id },
-        };
-        let order = [];
+        // const { lat, lng } = query;
+        // const id = decoded.id;
+        // const page = parseInt(query.page);
+        // const limit = 4;
+        // let group = ["sub_comment.id"];
+        // let search = {
+        //     where: { comment_id: comment_id },
+        // };
+        // let order = [];
 
-        order.push([Sequelize.literal("votes_total ASC")]);
+        // order.push([Sequelize.literal("votes_total ASC")]);
 
-        let attributes = [
-            "id",
-            "content",
-            "flags",
-            "is_flagged",
-            "createdAt",
-            "coordinates", [
-                Sequelize.literal(
-                    `(SELECT voted FROM sub_comment_votes WHERE user_id = ${id} AND sub_comment_id = sub_comment.id)`
-                ),
-                "vote",
-            ],
-            [
-                Sequelize.literal(
-                    `(SELECT CAST(SUM(voted) AS INT)  fROM sub_comment_votes WHERE sub_comment_id = sub_comment.id)`
-                ),
-                "votes_total",
-            ],
-        ];
-        let include = [{
-                model: models.user,
-                as: "creator",
-                attributes: {
-                    exclude: [
-                        "createdAt",
-                        "updatedAt",
-                        "deleted_at",
-                        "birthday",
-                        "reset_password_token",
-                        "reset_password_expiration",
-                        "refresh_token",
-                        "role_id",
-                        "bio",
-                        "type",
-                        "password",
-                    ],
-                },
-            },
-            {
-                model: models.user,
-                as: "replyTo",
-                attributes: {
-                    exclude: [
-                        "createdAt",
-                        "updatedAt",
-                        "deleted_at",
-                        "birthday",
-                        "reset_password_token",
-                        "reset_password_expiration",
-                        "refresh_token",
-                        "role_id",
-                        "bio",
-                        "type",
-                        "password",
-                        "email",
-                        "avatar",
-                        "active",
-                    ],
-                },
-            },
-            {
-                model: models.link,
-                as: "links",
-                required: false,
-                attributes: ["url"],
-                through: { attributes: [] },
-            },
-        ];
+        // let attributes = [
+        //     "id",
+        //     "content",
+        //     "flags",
+        //     "is_flagged",
+        //     "createdAt",
+        //     "coordinates", [
+        //         Sequelize.literal(
+        //             `(SELECT voted FROM sub_comment_votes WHERE user_id = ${id} AND sub_comment_id = sub_comment.id)`
+        //         ),
+        //         "vote",
+        //     ],
+        //     [
+        //         Sequelize.literal(
+        //             `(SELECT CAST(SUM(voted) AS INT)  fROM sub_comment_votes WHERE sub_comment_id = sub_comment.id)`
+        //         ),
+        //         "votes_total",
+        //     ],
+        // ];
+        // let include = [{
+        //         model: models.user,
+        //         as: "creator",
+        //         attributes: {
+        //             exclude: [
+        //                 "createdAt",
+        //                 "updatedAt",
+        //                 "deleted_at",
+        //                 "birthday",
+        //                 "reset_password_token",
+        //                 "reset_password_expiration",
+        //                 "refresh_token",
+        //                 "role_id",
+        //                 "bio",
+        //                 "type",
+        //                 "password",
+        //             ],
+        //         },
+        //     },
+        //     {
+        //         model: models.user,
+        //         as: "replyTo",
+        //         attributes: {
+        //             exclude: [
+        //                 "createdAt",
+        //                 "updatedAt",
+        //                 "deleted_at",
+        //                 "birthday",
+        //                 "reset_password_token",
+        //                 "reset_password_expiration",
+        //                 "refresh_token",
+        //                 "role_id",
+        //                 "bio",
+        //                 "type",
+        //                 "password",
+        //                 "email",
+        //                 "avatar",
+        //                 "active",
+        //             ],
+        //         },
+        //     },
+        //     {
+        //         model: models.link,
+        //         as: "links",
+        //         required: false,
+        //         attributes: ["url"],
+        //         through: { attributes: [] },
+        //     },
+        // ];
         const result = await models.sub_comment.findAll({
-            where: where
+            where: { comment_id: comment_id }
         });
         // const model = models.sub_comment;
         // const bagos = await paginate(
@@ -185,10 +185,11 @@ exports.index = async({ params, query, decoded }, res, next) => {
 
         return res.status(200).send(data);
     } catch (error) {
-        next(
-            ApiError.internalException("Não conseguiu se comunicar com o servidor")
-        );
-        return;
+        return res.status(500).send(error);
+        // next(
+        //     ApiError.internalException("Não conseguiu se comunicar com o servidor")
+        // );
+        // return;
     }
 };
 
