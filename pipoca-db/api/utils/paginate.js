a836af7356a5c37ca0f12a8196a2dc7d2627b8fb
rcsousa <rcsousa14@hotmail.com>
2021-05-19 14:23:25 +0100

    minor change to page and post 5

--- pipoca-db/api/utils/paginate.js
+++ pipoca-db/api/utils/paginate.js
@@ -78,7 +78,7 @@ exports.paginate = async(model, page, limit, search, order, attributes, include,
             "user_voted": row.vote ? true : false,
             "user_vote": row.vote == null ? null : row.vote,
             "user_isNear": isNear,
-            "reply_to": row.replyTo != null ? row.replyTo.username : "",
+            "reply_to": row.replyTo != null ? row.replyTo.username : null,
             //"distance": row.distance * 111, //km
             "info": {
                 "id": row.id,
