196d75476243a7a4e8f1d99a4cf1eb62a51b9cc4
rcsousa <rcsousa14@hotmail.com>
2021-05-21 10:44:29 +0100

    fixed sub_comment vote figure it out

--- pipoca-db/api/controllers/sub_comment_vote.js
+++ pipoca-db/api/controllers/sub_comment_vote.js
@@ -15,32 +15,32 @@ exports.store = async({ body, decoded }, res, next) => {
         const vote = await models.sub_comment_vote.findOne({
             where: { user_id: decoded.id, sub_comment_id: id },
         });
-        return res.status(200).json(vote);
-        // if (vote) {
-        //     await models.sub_comment_vote.update({ id: vote.id, user_id: decoded.id, voted, sub_comment_id: id }, {
-        //         where: { id: vote.id },
-        //     });
 
-        //     return res.status(200).json({
-        //         success: true,
-        //         message: "updated",
-        //     });
-        // }
-        // await models.sub_comment_vote.create({
-        //     user_id: decoded.id,
-        //     sub_comment_id: id,
-        //     voted,
-        // });
+        if (vote) {
+            await models.sub_comment_vote.update({ id: vote.id, user_id: decoded.id, voted, sub_comment_id: id }, {
+                where: { id: vote.id },
+            });
 
-        // return res.status(201).json({
-        //     success: true,
-        //     message: "added",
-        // });
+            return res.status(200).json({
+                success: true,
+                message: "updated",
+            });
+        }
+        await models.sub_comment_vote.create({
+            user_id: decoded.id,
+            sub_comment_id: id,
+            voted,
+        });
+
+        return res.status(201).json({
+            success: true,
+            message: "added",
+        });
     } catch (error) {
-        return res.status(500).json(error);
-        // next(
-        //     ApiError.internalException("Não conseguiu se comunicar com o servidor")
-        // );
-        // return;
+
+        next(
+            ApiError.internalException("Não conseguiu se comunicar com o servidor")
+        );
+        return;
     }
 };
--- pipoca-db/api/models/sub_comment_vote.js
+++ pipoca-db/api/models/sub_comment_vote.js
@@ -1,36 +1,36 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class sub_comment_vote extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      this.belongsTo(models.user, {
-        as:'user_sub_comment_vote',
-        foreignKey: 'user_id'
-      });
-      this.belongsTo(models.sub_comment,{
-        as: 'comment_sub_comment_vote',
-        foreignKey: 'subComment_id'
-        
+    class sub_comment_vote extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            this.belongsTo(models.user, {
+                as: 'user_sub_comment_vote',
+                foreignKey: 'user_id'
+            });
+            this.belongsTo(models.sub_comment, {
+                as: 'comment_sub_comment_vote',
+                foreignKey: 'subComment_id'
 
-      });
-      
-    }
-  };
-  sub_comment_vote.init({
-    user_id: DataTypes.INTEGER,
-    subComment_id: DataTypes.INTEGER,
-    voted: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'sub_comment_vote',
-  });
-  return sub_comment_vote;
+
+            });
+
+        }
+    };
+    sub_comment_vote.init({
+        user_id: DataTypes.INTEGER,
+        sub_comment_id: DataTypes.INTEGER,
+        voted: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'sub_comment_vote',
+    });
+    return sub_comment_vote;
 };
