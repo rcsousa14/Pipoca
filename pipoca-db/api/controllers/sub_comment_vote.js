4bb4c56cb2d5f4f1948b76cf56c1eb3e1e95119d
rcsousa <rcsousa14@hotmail.com>
2021-05-21 10:31:32 +0100

    fixed sub_comment vote 3

--- pipoca-db/api/controllers/sub_comment_vote.js
+++ pipoca-db/api/controllers/sub_comment_vote.js
@@ -5,35 +5,36 @@ const models = require("../models");
 exports.store = async({ body, decoded }, res, next) => {
     try {
         const { id, voted } = body;
-        const post = await models.sub_comment.findByPk(id);
-        if (!post) {
-            next(ApiError.badRequestException("Bago não foi encontrado!"));
-            return;
-        }
-        const vote = await models.sub_comment_vote.findOne({
-            where: { user_id: decoded.id, sub_comment_id: id },
-        });
+        return res.status(200).json(body);
+        // const post = await models.sub_comment.findByPk(id);
+        // if (!post) {
+        //     next(ApiError.badRequestException("Bago não foi encontrado!"));
+        //     return;
+        // }
+        // const vote = await models.sub_comment_vote.findOne({
+        //     where: { user_id: decoded.id, sub_comment_id: id },
+        // });
 
-        if (vote) {
-            await models.sub_comment_vote.update({ id: vote.id, user_id: decoded.id, voted, sub_comment_id: id }, {
-                where: { id: vote.id },
-            });
+        // if (vote) {
+        //     await models.sub_comment_vote.update({ id: vote.id, user_id: decoded.id, voted, sub_comment_id: id }, {
+        //         where: { id: vote.id },
+        //     });
 
-            return res.status(200).json({
-                success: true,
-                message: "updated",
-            });
-        }
-        await models.sub_comment_vote.create({
-            user_id: decoded.id,
-            sub_comment_id: id,
-            voted,
-        });
+        //     return res.status(200).json({
+        //         success: true,
+        //         message: "updated",
+        //     });
+        // }
+        // await models.sub_comment_vote.create({
+        //     user_id: decoded.id,
+        //     sub_comment_id: id,
+        //     voted,
+        // });
 
-        return res.status(201).json({
-            success: true,
-            message: "added",
-        });
+        // return res.status(201).json({
+        //     success: true,
+        //     message: "added",
+        // });
     } catch (error) {
         return res.status(500).json(error);
         // next(
