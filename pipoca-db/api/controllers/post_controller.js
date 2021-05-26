bc6c419c88cb0b3e981ac4f46d5088baa9d552d1
rcsousa <rcsousa14@hotmail.com>
2021-05-26 14:23:21 +0100

    found the problem with userVote

--- pipoca-db/api/controllers/post_controller.js
+++ pipoca-db/api/controllers/post_controller.js
@@ -115,8 +115,8 @@ exports.show = async({ params, query, decoded }, res, next) => {
         let newData = posts.get({ plain: true });
 
         let data = {
-            user_voted: newData['votes'] ? false : true,
-            user_vote: newData['votes'] == null ? null : newData['votes'],
+            user_voted: newData['vote'] ? false : true,
+            user_vote: newData['vote'] == null ? null : newData['vote'],
             user_isNear: isNear,
             reply_to: null,
             // distance: newData['distance'] * 111, //km
